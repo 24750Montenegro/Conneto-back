8fdc5b0869b7e633f17d22560b2168d0de9016ba
Luis-Angel-G <langelgirona@gmail.com>
2024-08-25 13:49:54 -0600

    Se elimina el atributo completada y el segundo constructor para concordar con el diagrama UML

--- conneto/conneto/src/main/java/com/uvg/conneto/models/Tarea.java
+++ conneto/conneto/src/main/java/com/uvg/conneto/models/Tarea.java
@@ -3,7 +3,7 @@ package com.uvg.conneto.models;
  * @ Project : Proyecto de Semestre, Conneto
  * @author Luis Girón
  * Creacion 10.08.2024
- * Ultima modificacion 10.08.2024
+ * Ultima modificacion 25.08.2024
  * @ File Name: Tarea.java
  * 
  */
@@ -16,16 +16,10 @@ public class Tarea {
     private boolean completada; // Un verificador si la tarea esta completa o no
 
     //Constructor 1: Método tradicional con parametros
-    public Tarea(String nombre, String descripcion, Usuario asignado, boolean completada){
+    public Tarea(String nombre, String descripcion, Usuario asignado){
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.asignado = asignado;
-		this.completada = completada;
-    }
-
-    //Constructor 2:  Metodo secundario sin parametros
-    public Tarea(){
-        //Iniicializa un objeto tarea, sin asignarle ningun valor a sus atributos.
     }
 
     //Set y get de nombre
