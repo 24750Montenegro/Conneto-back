390e1fcdff3b9b054d20b9f3f4c267ccee165da4
alemanuel18 <jer24678@uvg.edu.gt>
2024-09-01 10:41:20 -0600

    Se a creado la clase PublicacionControler.java

--- conneto/conneto/src/main/java/com/uvg/conneto/controllers/PublicacionControler.java
+++ conneto/conneto/src/main/java/com/uvg/conneto/controllers/PublicacionControler.java
@@ -4,19 +4,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import com.uvg.conneto.services.UsuarioService;
-import com.uvg.conneto.models.Usuario;
+import com.uvg.conneto.services.PublicacionService;
+import com.uvg.conneto.models.Publicacion;
 
 import java.util.ArrayList;
 
 @RestController
-@RequestMapping("/publicación")
+@RequestMapping("/publicacion")
 public class PublicacionControler {
 
-    
+    @Autowired
+    private PublicacionService publicacionService;
+
+    @GetMapping()
+    public ArrayList<Publicacion> obtenerPublicaciones(){
+        return publicacionService.obtenerPublicaciones();
+    }
+
+    @PostMapping()
+    public Publicacion guardarPublicacion(@RequestBody Publicacion publicacion){
+        return this.publicacionService.guardarPublicacion(publicacion);
+    }
     
 }
