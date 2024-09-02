145eee86d0128038d32fbd0ea44a16d2c2f4e0b5
Luis-Angel-G <langelgirona@gmail.com>
2024-09-01 18:07:13 -0600

    Se crea TareaController.java

---
+++ conneto/conneto/src/main/java/com/uvg/conneto/controllers/TareaController.java
@@ -0,0 +1,32 @@
+package com.uvg.conneto.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.uvg.conneto.services.UsuarioService;
+import com.uvg.conneto.models.Tarea;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/tarea")
+public class TareaController {
+
+    @Autowired
+    private TareaService tareaService;
+
+    @GetMapping()
+    public ArrayList<Tarea> obtenerTareas() {
+        return tareaService.obtenerTareas();
+    }
+
+    @PostMapping()
+    public Tarea guardarTarea(@RequestBody Tarea tarea) {
+        return this.tareaService.guardarTarea(tarea);
+    }
+}
