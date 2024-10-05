63a657068f8db892ebcfcfde818f05f2989f4506
alemanuel18 <jer24678@uvg.edu.gt>
2024-10-05 17:49:50 -0600

    Se creo la clase ComentarioService

---
+++ conneto/conneto/src/main/java/com/uvg/conneto/services/ComentarioService.java
@@ -0,0 +1,17 @@
+package com.uvg.conneto.services;
+import org.springframework.stereotype.Service;
+
+import com.uvg.conneto.models.Comentario;
+import com.uvg.conneto.repositories.ComentarioRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ComentarioService {
+
+    private final ComentarioRepository comentarioRepository;
+    public void crearteComentario(Comentario comentario){
+        comentarioRepository.save(comentario);
+    }
+}
