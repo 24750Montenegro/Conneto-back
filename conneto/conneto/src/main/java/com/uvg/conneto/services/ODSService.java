89fb1d3f0f4b176435ad6aa2feb439fe40ae86c2
Marcelo Detlefsen <83415711+MarceloDetlefsen@users.noreply.github.com>
2024-10-08 17:53:07 -0600

    Se creó la clase ODSService

---
+++ conneto/conneto/src/main/java/com/uvg/conneto/services/ODSService.java
@@ -0,0 +1,19 @@
+package com.uvg.conneto.services;
+import org.springframework.stereotype.Service;
+
+import com.uvg.conneto.models.ODS;
+import com.uvg.conneto.repositories.ODSRepository;
+
+import lombok.RequiredArgsConstructor;
+
+
+@Service
+@RequiredArgsConstructor
+public class ODSService 
+{
+    private final ODSRepository odsRepository;
+    public void crearteComentario(ODS ods)
+    {
+        odsRepository.save(ods);
+    }
+}
