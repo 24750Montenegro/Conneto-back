ff37df1891ed6796bbc435ac7d211508d37d80ee
Marcelo Detlefsen <83415711+MarceloDetlefsen@users.noreply.github.com>
2024-10-08 21:01:11 -0600

    Se creó ODSController y se generaron las funciones CRUD

--- conneto/conneto/src/main/java/com/uvg/conneto/services/ODSService.java
+++ conneto/conneto/src/main/java/com/uvg/conneto/services/ODSService.java
@@ -1,4 +1,7 @@
 package com.uvg.conneto.services;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import com.uvg.conneto.models.ODS;
@@ -10,9 +13,40 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ODSService 
 {
+    //Crear ODS
     private final ODSRepository odsRepository;
     public void crearODS(ODS ods)
     {
         odsRepository.save(ods);
+    }
+
+    // Leer ODS por ID
+    public Optional<ODS> obtenerODSPorId(Long id) 
+    {
+        return odsRepository.findById(id);
+    }
+
+    // Leer todas las ODS
+    public List<ODS> obtenerTodasODS() 
+    {
+        return odsRepository.findAll();
+    }
+
+    //Actualizar ODS
+    public void actualizarODS(Long id, ODS odsActualizado) 
+    {
+        Optional<ODS> odsExistente = odsRepository.findById(id);
+        if (odsExistente.isPresent()) {
+            ODS ods = odsExistente.get();
+            ods.setNombre(odsActualizado.getNombre());
+            ods.setDescripcion(odsActualizado.getDescripcion());
+            odsRepository.save(ods);
+        }
+    }
+
+    //Eliminar ODS
+    public void eliminarODS(Long id) 
+    {
+        odsRepository.deleteById(id);
     }
 }
