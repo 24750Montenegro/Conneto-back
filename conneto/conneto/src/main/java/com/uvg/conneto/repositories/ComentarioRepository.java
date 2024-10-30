0ff92cc497d5a02aeed8b67eb6b8bfff2c537a27
alemanuel18 <jer24678@uvg.edu.gt>
2024-10-29 18:52:07 -0600

    Se a creado la conexion del back con el front, falta comprobar su funcionamiento al momento de que publicion este conectado al back

--- conneto/conneto/src/main/java/com/uvg/conneto/repositories/ComentarioRepository.java
+++ conneto/conneto/src/main/java/com/uvg/conneto/repositories/ComentarioRepository.java
@@ -1,12 +1,13 @@
+// ComentarioRepository.java
 package com.uvg.conneto.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.uvg.conneto.models.Comentario;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
 
 @Repository
-public interface ComentarioRepository extends JpaRepository<Comentario, Long>
-{
-    Comentario findById(long id);
+public interface ComentarioRepository extends JpaRepository<Comentario, Long> {
+    List<Comentario> findByPublicacionId(Long publicacionId, Pageable pageable);
 }
--- conneto/conneto/src/main/java/com/uvg/conneto/services/ComentarioService.java
+++ conneto/conneto/src/main/java/com/uvg/conneto/services/ComentarioService.java
@@ -1,18 +1,39 @@
+// ComentarioService.java
 package com.uvg.conneto.services;
-import org.springframework.stereotype.Service;
 
+import org.springframework.stereotype.Service;
 import com.uvg.conneto.models.Comentario;
 import com.uvg.conneto.repositories.ComentarioRepository;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import java.util.stream.Collectors;
+import java.util.Map;
+import java.util.HashMap;
 
 @Service
 @RequiredArgsConstructor
 public class ComentarioService {
 
     private final ComentarioRepository comentarioRepository;
-    public void createComentario(Comentario comentario){
+
+    public void createComentario(Comentario comentario) {
         comentarioRepository.save(comentario);
     }
-    
+
+    public List<Map<String, Object>> obtenerComentariosPorPublicacion(Long publicacionId, int cantidad) {
+    List<Comentario> comentarios = comentarioRepository.findByPublicacionId(publicacionId, PageRequest.of(0, cantidad));
+
+    return comentarios.stream().map(comentario -> {
+        Map<String, Object> comentarioMap = new HashMap<>();
+        comentarioMap.put("id", comentario.getId());
+        comentarioMap.put("username", comentario.getAutor().getNombre()); // Ajusta al nombre del autor
+        comentarioMap.put("text", comentario.getContenido()); // Ajusta al contenido del comentario
+        return comentarioMap;
+    }).collect(Collectors.toList());
+}
 }
