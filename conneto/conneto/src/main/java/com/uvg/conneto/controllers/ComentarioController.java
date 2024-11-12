3741a43e9ecd90c75ccc97bd308620fb5b3139b9
Marcelo Detlefsen <83415711+MarceloDetlefsen@users.noreply.github.com>
2024-11-11 18:58:47 -0600

    Se realizo un merge de Marcelo a Dev agregando atributos en Publicación.

--- conneto/conneto/src/main/java/com/uvg/conneto/controllers/ComentarioController.java
+++ conneto/conneto/src/main/java/com/uvg/conneto/controllers/ComentarioController.java
@@ -2,18 +2,13 @@ package com.uvg.conneto.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
 
-import com.uvg.conneto.services.AlianzaService;
-import com.uvg.conneto.services.ComentarioService;
-import com.uvg.conneto.models.Alianza;
 import com.uvg.conneto.models.Comentario;
-
-import java.util.ArrayList;
+import com.uvg.conneto.services.ComentarioService;
 
 
 @RestController
--- conneto/conneto/src/main/java/com/uvg/conneto/controllers/UsuarioController.java
+++ conneto/conneto/src/main/java/com/uvg/conneto/controllers/UsuarioController.java
@@ -1,18 +1,18 @@
 package com.uvg.conneto.controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody;
 
-import com.uvg.conneto.services.UsuarioService;
 import com.uvg.conneto.models.Usuario;
-
-import java.util.ArrayList;
+import com.uvg.conneto.services.UsuarioService;
 
 @RestController
 @RequestMapping("/usuario")
@@ -31,13 +31,13 @@ public class UsuarioController {
         return this.usuarioService.guardarUsuario(usuario);
     }
 
-    @CrossOrigin(origins = "https://conneto-front.vercel.app")
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/signup")
     public Usuario signUp(@RequestBody Usuario usuario) {
         return usuarioService.registrarUsuario(usuario);
     }
 
-    @CrossOrigin(origins = "https://conneto-front.vercel.app")
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/login")
     public Usuario login(@RequestParam String email, @RequestParam String contrasena) {
         return usuarioService.autenticarUsuario(email, contrasena);
