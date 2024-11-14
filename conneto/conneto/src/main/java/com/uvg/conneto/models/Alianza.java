bf65ee598089bd1a970be2114cab3fafcd73739b
alemanuel18 <jer24678@uvg.edu.gt>
2024-11-14 10:57:38 -0600

    Se conecto aliana correctamente con el front pudiendo crear alianza, ver todas las alianzas y unirse a una alianza

--- conneto/conneto/src/main/java/com/uvg/conneto/models/Alianza.java
+++ conneto/conneto/src/main/java/com/uvg/conneto/models/Alianza.java
@@ -2,6 +2,8 @@ package com.uvg.conneto.models;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 //  @ Proyecto Fase 2
 //  @ File Name : Alianza.java
 //  @ Date : 10/08/2024
@@ -32,6 +34,7 @@ public class Alianza {
         joinColumns = @JoinColumn(name = "alianza_id"),
         inverseJoinColumns = @JoinColumn(name = "usuario_id")
     )
+    @JsonIgnoreProperties(value= {"publicaciones", "proyectos","aliados","alianzas","interesesODS","contrasena","publicacionesQueLeGustan","habilidades","email","ubicacion"})
     private List<Usuario> usuarios;
 
     @OneToMany(mappedBy = "alianza", cascade = CascadeType.ALL)
