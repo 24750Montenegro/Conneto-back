2ad24db6f97310de822625526febc0de33ff3a8b
Luis-Angel-G <langelgirona@gmail.com>
2024-11-17 21:03:20 -0600

    Se documenta tarea

--- conneto/conneto/src/main/java/com/uvg/conneto/models/Tarea.java
+++ conneto/conneto/src/main/java/com/uvg/conneto/models/Tarea.java
@@ -1,3 +1,4 @@
+// Definimos el paquete donde se encuentra la clase Tarea
 package com.uvg.conneto.models;
 /**
  * @ Project : Proyecto de Semestre, Conneto
@@ -7,25 +8,32 @@ package com.uvg.conneto.models;
  * @ File Name: Tarea.java
  * 
  */
-
-
  import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
- 
+ // Se importan las anotaciones de JPA necesarias para la configuración de la entidad y las relaciones en la base de datos
  import jakarta.persistence.*;
  import lombok.AllArgsConstructor;
  import lombok.Data;
  import lombok.NoArgsConstructor;
  
+ //Genera getters correspondientes y demás métodos como toString
  @Data
+ //Genera un constructor con todos los parametros
  @AllArgsConstructor
+ //Genera un constructor sin parametros
  @NoArgsConstructor
+ //Se define a la clase como una entidad para la persistencia de datos
  @Entity
+ //Se crea la clase tarea
 public class Tarea {
 
+    // Atributos de la clase
+    //Se define Id como la clave primaria de la entidad
     // Atributos
     @Id 
+    //Hace que el Id sea autogenerado en la base de datos
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     @Column(unique = true, nullable = false)
+    //Atributo privado id
     private Long id; 
 
     @Basic
@@ -33,16 +41,17 @@ public class Tarea {
     private String descripcion; // una breve descripcion de la tarea a realizar
     private boolean completada; // Un verificador si la tarea esta completa o no
 
-
+    //Multiplicidad varios a uno, cada tarea tiene un usuario y un usuario varias tareas
     @ManyToOne
     @JsonIgnoreProperties(value = {"publicacionesQueLeGustan", "publicaciones", "likes"})
-    @JoinColumn(name = "asignado_id")
+    @JoinColumn(name = "asignado_id") // Llave foránea que relaciona la tarea con un usuario
     private Usuario asignado; // El usuario que está asignado para realizar la tarea
-    
+
+    //Multiplicidad varios a uno, cada tarea tiene un proyecto y un proyecto varias tareas
     @ManyToOne
     @JsonIgnoreProperties(value= {"autor", "usuarios","tareas", "alianza"})
-    @JoinColumn(name = "proyecto_id")
-    private Proyecto proyecto;
+    @JoinColumn(name = "proyecto_id") // Llave foránea que relaciona la tarea con un proyecto
+    private Proyecto proyecto; // El proyecto en el que se encuentra la tarea
 
     //Constructor 1: Método tradicional con parametros
     public Tarea(String nombre, String descripcion, Usuario asignado, boolean completada){
