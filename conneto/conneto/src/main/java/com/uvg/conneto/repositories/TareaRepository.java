d12b65737ca016541e4d3eb39aa303372a5a33d6
Luis-Angel-G <langelgirona@gmail.com>
2024-11-17 21:06:48 -0600

    Se documenta TareaRepository

--- conneto/conneto/src/main/java/com/uvg/conneto/repositories/TareaRepository.java
+++ conneto/conneto/src/main/java/com/uvg/conneto/repositories/TareaRepository.java
@@ -1,17 +1,27 @@
+//Se indica donde se guarda la TareaRepository
 package com.uvg.conneto.repositories;
 
-
-
+//Se importan los recursos necesarios
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
 import com.uvg.conneto.models.Tarea;
 
+/**
+ * TareaRepository es una interfaz que extiende {@link CrudRepository} para proporcionar operaciones
+ * CRUD (crear, leer, actualizar y eliminar) en entidades {@link Tarea}.
+ * @Repository indica que esta interfaz es un componente de repositorio de Spring,
+ * lo que permite que sea detectada automáticamente y manejada por el contenedor de Spring.
+ */
 @Repository
 public interface TareaRepository extends JpaRepository<Tarea, Long> {
+        /**
+         * Busca y devuelve una tarea basada en su nombre.
+         *
+         * @param nombre el nombre de la tarea a buscar
+         * @return la tarea con el nombre especificado, o {@code null} si no se encuentra ninguno.
+         */
         Tarea findByNombre(String nombre);
         List<Tarea> findByProyectoId(Long proyectoId);
-
 }
